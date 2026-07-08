8ca934ace5316c4c6274b8ea94ce9e8b2f3a7f1e
Kazuki Matsuo <kazuki.matsuo.728@gmail.com>
2026-07-08 21:21:27 +0900

    cap observability and argocd memory usage

--- hosts/n150/configuration.nix
+++ hosts/n150/configuration.nix
@@ -282,6 +282,7 @@ in
       ExecStart = "${pkgs.opentelemetry-collector-contrib}/bin/otelcol-contrib --config /etc/otelcol/config.yaml";
       Restart = "on-failure";
       RestartSec = "5s";
+      MemoryMax = "256M";
       StateDirectory = "otelcol";
       StateDirectoryMode = "0750";
     };
